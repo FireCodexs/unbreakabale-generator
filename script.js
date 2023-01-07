52733053942ec3ea9151b6f47f9ae3db20f0b814
Mihai-Andrei Grigoras <andrei.eu.mihaii@gmail.com>
2023-01-07 16:50:57 +0000

    added a reset button

--- script.js
+++ script.js
@@ -136,16 +136,16 @@ function generatePassword() {
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+let generateBtn = document.querySelector('#generate');
+let reset = document.querySelector('#reset');
 
 // Write password to the #password input
 function writePassword() {
   getRandom();
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  let password = generatePassword();
+  let passwordText = document.querySelector('#password');
   passwordText.value = password;
 }
-
 
 
 // Add event listener to generate button
