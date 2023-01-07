5f49b45ff4f2a70e5e509845b49d65d743517322
Mihai-Andrei Grigoras <andrei.eu.mihaii@gmail.com>
2023-01-07 15:52:43 +0000

    restructured html and added event listeners

--- script.js
+++ script.js
@@ -88,16 +88,48 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Function to prompt user for password options
-function getPasswordOptions() {
+//Here we will store the values of the user preferences
+var bigArray = []; //This is going to store the user preferences for the password
+var randomArray = []; //This is going to store the randomly assinged elements of the password
+let lowercaseCheckbox = document.querySelector("input[name=lowercase]");
+let uppercaseCheckbox = document.querySelector("input[name=uppercase]");
+let numbersCheckbox = document.querySelector("input[name=numbers]");
+let specialsCheckbox = document.querySelector("input[name=specials]");
 
-}
+//Here we will register click event listeners / Add values to the array when user clicks
+//TO ADD: Validation of buttons
 
+  lowercaseCheckbox.addEventListener('click', function(){
+    if(this.checked) {
+      bigArray = bigArray.concat(lowerCasedCharacters)
+    }
+    console.log(bigArray)
+  })
+  uppercaseCheckbox.addEventListener('change', function(){
+    if(this.checked) {
+      bigArray = bigArray.concat(upperCasedCharacters)
+    }
+  })
+  numbersCheckbox.addEventListener('change', function(){
+    if(this.checked) {
+      bigArray = bigArray.concat(numericCharacters)
+    }
+  })
+  specialsCheckbox.addEventListener('change', function(){
+    if(this.checked) {
+      bigArray = bigArray.concat(specialCharacters)
+    }
+  })
+
+console.log(rangeValue.innerText)
 // Function for getting a random element from an array
-function getRandom(arr) {
-
+function getRandom() {
+  for(i=0;i<bigArray.length;i++){
+    randomArray.push(bigArray[Math.floor(Math.random() * bigArray.length + 1)]);//this will give us a random number that we will use to index
+  }
+  console.log(randomArray)
 }
-
+getRandom();
 // Function to generate password with user input
 function generatePassword() {
 
@@ -113,6 +145,8 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
